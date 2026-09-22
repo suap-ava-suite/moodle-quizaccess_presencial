bb211d724428a25961564e5bf1f7b69648e2f9fd
danielxlves <dxnielalves@gmail.com>
2026-09-22 10:07:54 -0300

    Fix Moodle coding style violations

--- classes/admin_setting/positive_integer.php
+++ classes/admin_setting/positive_integer.php
@@ -8,15 +8,13 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace quizaccess_presencial\admin_setting;
-
-defined('MOODLE_INTERNAL') || die();
 
 /**
  * Administration setting that accepts positive whole integers only.
